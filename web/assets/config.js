a00aa8d9ec97cde5356b0965eb5ae338364daf3e
Kato <katobalano@gmail.com>
2026-05-23 12:53:15 +0700

    Edit web/*.html relative path to config.js file

--- web/assets/config.js
+++ web/assets/config.js
@@ -1,5 +1,5 @@
 // web/assets/config.js
 const CONFIG = {
-  API_BASE_URL: 'https://pccc-api.giavico.com.vn/api',
+  API_BASE_URL: 'https://api-pccc.giavico.com.vn/api',
   WEB_BASE_URL: 'https://pccc.giavico.com.vn',
 };
